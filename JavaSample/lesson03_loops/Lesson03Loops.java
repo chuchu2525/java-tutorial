38bf0d22adb0e0864bc44f231a098690d88bbf2b
chuchu2525 <n.yuu.dev@gmail.com>
2026-04-08 21:52:46 +0900

    for とwhileわかりやすいように修正

--- JavaSample/lesson03_loops/Lesson03Loops.java
+++ JavaSample/lesson03_loops/Lesson03Loops.java
@@ -2,16 +2,21 @@ package JavaSample.lesson03_loops;
 
 public class Lesson03Loops {
     public static void main(String[] args) {
-        System.out.println("for文");
-        for (int i = 1; i <= 3; i++) {
-            System.out.println(i + "回目");
+        System.out.println("=== for文: 回数が決まっている繰り返し ===");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("for -> " + i);
         }
 
-        System.out.println("while文");
-        int count = 1;
-        while (count <= 3) {
-            System.out.println(count + "回目");
-            count++;
+        System.out.println();
+        System.out.println("=== while文: 条件を満たす間の繰り返し ===");
+        int sum = 0;
+        int n = 1;
+        while (sum <= 20) {
+            sum += n;
+            System.out.println("while -> n=" + n + ", sum=" + sum);
+            n++;
         }
+
+        System.out.println("終了時点: n=" + n + ", sum=" + sum);
     }
 }
